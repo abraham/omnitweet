14c355bb962468454d272d801f9301af2e194def
abraham <4braham@gmail.com>
2010-08-27 16:10:56 -0700

    new status notifications changed to html with links. closes #7

---
+++ extension/status.js
@@ -0,0 +1,16 @@
+// Work with statuses
+var Status = (function ()  {
+  var my = {};
+  
+  // Save a status object to localStorage
+  my.save = function(status){
+  return localStorage['status_' + status.id] = JSON.stringify(status);
+  }
+  
+  // Get the status object matching the id
+  my.get = function(id) {
+    console.log(typeof JSON.parse(localStorage['status_' + id]))
+    return JSON.parse(localStorage['status_' + id]);
+  }
+  return my;
+}());

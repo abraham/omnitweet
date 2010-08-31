cf18f78558a9ae749029a1bcb92e03687ee3d312
abraham <4braham@gmail.com>
2010-08-31 04:28:17 -0700

    added mentions and direct_messages methods to Twitter.

--- extension/javascript/twitter.js
+++ extension/javascript/twitter.js
@@ -1,6 +1,55 @@
 // Make requests to Twitter
 var Twitter = (function ()  {
   var my = {};
+  my.current_requests = {mentions: false, direct_messages: false};
+
+  // Get mentions
+  my.mentions = function(init) {
+    if (my.current_requests.mentions) {
+      return false;
+    }
+    var callback = function(result) {
+      if (200 == result.status && result.data.length){
+        localStorage.statuses_last_id = result.data[0].id;
+        for(var status in result.data) {
+          Status.save(result.data[status]);
+          Notice.status(result.data[status].id);
+        }
+      }
+      my.current_requests.mentions = false;
+      console.log('Pulled new mentions :)');
+    };
+    var parameters = {};
+    if (localStorage.statuses_last_id) {
+      parameters.since_id = localStorage.statuses_last_id;
+    }
+    // Make request to Twitter for mentions.
+    Twitter.get('statuses/mentions', callback, parameters);
+  }
+  
+  // Get direct messages
+  my.direct_messages = function(init) {
+    if (my.current_requests.direct_messages) {
+      return false;
+    }
+    var callback = function(result) {
+      if (200 == result.status && result.data.length){
+        localStorage.direct_messages_last_id = result.data[0].id;
+        for(var direct_messages in result.data) {
+          DirectMessage.save(result.data[direct_messages]);
+          Notice.direct_message(result.data[direct_messages].id);
+        }
+      }
+      my.current_requests.direct_messages = false;
+      console.log('Pulled new direct message :)');
+    };
+    var parameters = {};
+    if (localStorage.direct_messages_last_id) {
+      parameters.since_id = localStorage.direct_messages_last_id;
+    }
+    // Make request to Twitter for direct messages.
+    Twitter.get('direct_messages', callback, parameters);
+  }
   
   // Make a GET request.
   my.get = function(path, callback, parameters) {
